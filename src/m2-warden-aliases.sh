5dba680d1037447c18ecc8a71e6cb3ff9a5f0417
Raj KB <rajendra.bhatta@citruss.com>
2021-09-16 22:12:25 +0400

    Add aliases for db, redis, sh, env

--- src/m2-warden-aliases.sh
+++ src/m2-warden-aliases.sh
@@ -1,24 +1,39 @@
+wardenDebug() {
+	warden debug
+}
+alias wdb="wardenDebug"
+
+wardenShell() {
+	warden shell $1
+}
+alias wsh="wardenShell"
+
+wardenShellM2() {
+	warden shell -c "bin/magento $1"
+}
+alias wm2="wardenShellM2"
+
 wardenShellMageRun() {
 	warden shell -c "n98-magerun $1"
 }
 alias wmr="wardenShellMageRun"
 
-wardenShellM2() {
-	warden shell -c "bin/magento $1"
-}
-alias wm="wardenShellM2"
-
 wardenRedis() {
 	warden redis $1
 }
-alias wr="wardenRedis"
+alias wrd="wardenRedis"
 
 wardenDb() {
 	warden db $1
 }
-alias wd="wardenDb"
+alias wdb="wardenDb"
 
 wardenLogs() {
 	warden env logs --tail 0 -f nginx php-fpm php-debug
 }
-alias wl="wardenLogs"
+alias wlg="wardenLogs"
+
+wardenEnv() {
+	warden env $1
+}
+alias wenv="wardenEnv"
